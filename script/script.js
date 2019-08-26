9f64118830b4026b9e20953786b558c4a13e2dd5
Nata <natalikrasnobaieva@gmail.com>
2019-08-26 16:10:53 +0300

    fixed

--- script/script.js
+++ script/script.js
@@ -21,15 +21,11 @@ switch (lang) {
 }
 console.log(arr);
 
-switch (lang) {
-    case 'ru':
-       arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-    break;
-    case 'en':
-        arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-    break;
-}
-console.log(arr);
+ arr = {
+	'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
+	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+};
+console.log(arr[lang]);
 
 let namePerson;
 
