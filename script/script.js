4921b4d3fdd402b2061b7fa6fbd2bcf6c992bec0
Nata <natalikrasnobaieva@gmail.com>
2019-08-26 14:58:48 +0300

    dop done

---
+++ script/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let lang = 'ru';
+let arr;
+
+if (lang == 'ru') {
+     arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+}
+if (lang == 'en') {
+    arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+}
+console.log(arr);
+
+switch (lang) {
+    case 'ru':
+     arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+    break;
+    case 'en':
+        arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+    break;
+}
+console.log(arr);
+
+switch (lang) {
+    case 'ru':
+       arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+    break;
+    case 'en':
+        arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+    break;
+}
+console.log(arr);
+
+let namePerson;
+
+namePerson === 'Артем' ? console.log('директор') : console.log('студент');
+namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
