420134ebae0808cc756209023181513708c30301
Nata <natalikrasnobaieva@gmail.com>
2019-08-30 18:46:58 +0300

    fixed

--- script/script.js
+++ script/script.js
@@ -2,15 +2,14 @@
 
 function checkType(data){
     if (typeof data != 'string'){
-        document.write('Это не строка');
+        console.log('Это не строка');
     } else if (data.length > 30){
-        data.slice(0, -1);
-        data + '...';
-       return data.slice(0, 30).trim() + '...';
-        return data.trim();
+        
+        return data.trim().slice(0, 30) + '...';
+
     } else {
         return data.trim();
     }
 
 }
-console.log(checkType(' 01234567891011121314151251461718192021222324252627282930'));
+console.log(checkType('                                                           01234567891011121314151251461718192021222324252627282930'));
