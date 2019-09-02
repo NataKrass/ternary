c9c8357a6c00308f7b793b1a56e34222edd5f574
Nata <natalikrasnobaieva@gmail.com>
2019-09-02 11:46:09 +0300

    done

--- script/script.js
+++ script/script.js
@@ -1,15 +1,17 @@
 'use strict';
 
-function checkType(data){
-    if (typeof data != 'string'){
-        console.log('Это не строка');
-    } else if (data.length > 30){
-        
-        return data.trim().slice(0, 30) + '...';
+let arr = ['245', '5678', '458', '355', '325', '789', '4905'];
 
-    } else {
-        return data.trim();
+ arr.forEach((e) => {
+   if (e[0] == '2' || e[0] == '4') {
+    console.log(e);
+   }
+ });
+
+
+Function: for ( let i = 2; i <= 100; i++){
+    for (let n = 2; n < i; n++) {
+        if (i % n == 0) continue Function;
     }
-
+    console.log(i + ' Делители этого числа: 1 и ' + i)
 }
-console.log(checkType('                                                           01234567891011121314151251461718192021222324252627282930'));
